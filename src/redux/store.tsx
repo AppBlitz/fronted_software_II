acc6b9263519ca2c8497affeb513e0948f219da0
carlos <karlcorralesmovil@gmail.com>
2024-10-24 12:03:01 -0500

    Descripción de tus cambios

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,15 +1,15 @@
-import { productSlice, userSlice } from "./slice";
+import userReducer from "./slice/userSlice.tsx";
+import productReducer from "./slice/ProductSlice.tsx"
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
-    user: userSlice.reducer,
-    product: productSlice.reducer,
+    user: userReducer,
+    product: productReducer
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
 export { store };
